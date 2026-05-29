c189ad0df4e87a06b4b5ea2f8d77e9557217ed60
Cocobarce <josebarcelodawi@gmail.com>
2026-05-29 10:46:19 -0300

    termine score UI recoleccion y cuerpos

---
+++ Assets/SCRIPTS/ColDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ColDetection : MonoBehaviour
+{
+    public int contador = 0;
+    public Text contadorText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contadorText.text = "Score: " + contador;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other){
+        if (other.CompareTag("celu")){
+            Destroy(other.gameObject);
+            contador++;
+            contadorText.text = "Score: " + contador;
+        }
+        
+    }
+}
